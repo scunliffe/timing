35ff5f2bee0b0be9e6450cbdfae5b991915ea37a
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2014-09-26 16:42:27 -0400

    Update bookmarklet.js
    Changed to a local reference, removed redundant script type attribute

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
-(function() {
+(function(){
 	if(!window.__profiler||window.__profiler.scriptLoaded!==true){
 		var d=document,
 				h=d.getElementsByTagName('head')[0],
@@ -12,10 +12,9 @@
 					window.__profiler.init();
 					__profiler.scriptLoaded=true;
 				},
-		s.type='text/javascript';
-		l.style.cssText='z-index:999;position:fixed;top:10px;left:10px;display:inline;width:auto;font-size:14px;line-height:1.5em;font-family:Helvetica,Calibri,Arial,sans-serif;text-shadow:none;padding:3px 10px 0;background:#FFFDF2;box-shadow:0 0 0 3px rgba(0,0,0,.25),0 0 5px 5px rgba(0,0,0,.25); border-radius:1px';
+		l.style.cssText='z-index:999;position:fixed;top:10px;left:10px;display:inline;width:auto;font-size:14px;line-height:1.5em;font-family:Arial,Helvetica,Calibri,sans-serif;text-shadow:none;padding:3px 10px 0;background:#fffdf2;box-shadow:0 0 0 3px rgba(0,0,0,.25),0 0 5px 5px rgba(0,0,0,.25); border-radius:1px';
 		l.innerHTML='Just a moment';
-		s.src='//kaaes.github.io/timing/profiler.js';
+		s.src='//scunliffe.github.io/timing/profiler.js';
 		s.onload=c;
 		s.onreadystatechange=function(){if(this.readyState=='loaded'){c()}};
 		d.body.appendChild(l);
